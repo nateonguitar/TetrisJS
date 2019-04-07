29ef42089a227f907723c59166a52ae1be1e976d
Nasser Dos <nasserbinshabeeb@mail.weber.edu>
2019-04-07 12:34:00 -0700

    all vector2 functions that exist in the outlined repo

--- lib/Vector2.ts
+++ lib/Vector2.ts
@@ -1,4 +1,4 @@
-// TODO: implement some of this functionality.  All we did was zero to start out with
+// TODO: implement some of functionality.  All we did was zero to start out with
 //       https://gist.github.com/Dalimil/3daf2a0c531d7d030deb37a7bfeff454
 class Vector2 {
 	public x: number;
@@ -9,8 +9,81 @@ class Vector2 {
 		this.y = y;
 	}
 
-	/** Returns a Vector2 with x=0, y=0 */
 	static get zero(): Vector2 {
 		return new Vector2(0, 0);
+	}
+
+	public clone(): Vector2 {
+		return new Vector2(this.x, this.y)
+	}
+
+	public add(vector) {
+		return new Vector2(this.x + vector.x, this.y + vector.y);
+	}
+
+	public subtract(vector) {
+		return new Vector2(this.x - vector.x, this.y - vector.y);
+	}
+
+	public scale(scalar) {
+		return new Vector2(this.x * scalar, this.y * scalar);
+	}
+
+	public dot(vector) {
+		return (this.x * vector.x + this.y + vector.y);
+	}
+
+	public moveTowards(vector, t) {
+		t = Math.min(t, 1);
+		var diff = vector.subtract(this);
+		return this.add(diff.scale(t));
+	}
+
+	public magnitude() {
+		return Math.sqrt(this.magnitudeSqr());
+	}
+
+	public magnitudeSqr() {
+		return (this.x * this.x + this.y * this.y);
+	}
+
+	public distance(vector) {
+		return Math.sqrt(this.distanceSqr(vector));
+	}
+
+	public distanceSqr(vector) {
+		var deltaX = this.x - vector.x;
+		var deltaY = this.y - vector.y;
+		return (deltaX * deltaX + deltaY * deltaY);
+	}
+
+	public normalize() {
+		var mag = this.magnitude();
+		var vector = this.clone();
+		if (Math.abs(mag) < 1e-9) {
+			vector.x = 0;
+			vector.y = 0;
+		} else {
+			vector.x /= mag;
+			vector.y /= mag;
+		}
+		return vector;
+	}
+
+	public angle() {
+		return Math.atan2(this.y, this.x);
+	}
+
+	public rotate(alpha) {
+		var cos = Math.cos(alpha);
+		var sin = Math.sin(alpha);
+		var vector = new Vector2(0, 0);
+		vector.x = this.x * cos - this.y * sin;
+		vector.y = this.x * sin + this.y * cos;
+		return vector;
+	}
+
+	public toString() {
+		return ("[" + this.x + "; " + this.y + "]");
 	}
 }
