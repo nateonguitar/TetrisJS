037a7639818930b1021f0e1ee7a87641400fa4b6
Nate Brooks <nateonguitar@gmail.com>
2019-05-26 20:23:18 -0600

    colors class level

--- Frogger/GameObjects/FroggerRiver.ts
+++ Frogger/GameObjects/FroggerRiver.ts
@@ -1,4 +1,10 @@
 class FroggerRiver extends GameObject {
+
+	private blue: string     = "#003388";
+	private darkBlue: string = "#002244";
+	private brown: string    = "#654321";
+	private black: string    = "#000000";
+
 	constructor(levelHeight:number) {
 		super({layer: 0});
 		let o = GameManager.options;
@@ -6,23 +12,19 @@ class FroggerRiver extends GameObject {
 	}
 
 	public draw(): void {
-		let blue     = "#003388";
-		let darkBlue = "#002244";
-		let brown    = "#654321";
-		let black    = "#000000";
 
 		// draw river water
-		Canvas.setFillStyle(blue);
+		Canvas.setFillStyle(this.blue);
 		Canvas.fillRect(0, 0, this.transform.size.x, this.transform.size.y);
 
 		// draw banks
-		Canvas.setFillStyle(brown);
+		Canvas.setFillStyle(this.brown);
 		Canvas.fillRect(0, 0, this.transform.size.x, FroggerMainLevelController.unitHeight);
 		let bank2TopLeft = new Vector2(0, this.transform.size.y - FroggerMainLevelController.unitHeight);
 		let bank2Size = new Vector2(this.transform.size.x, FroggerMainLevelController.unitHeight);
 		Canvas.fillRect(bank2TopLeft.x, bank2TopLeft.y, bank2Size.x, bank2Size.y);
 
-		Canvas.setFillStyle(darkBlue);
+		Canvas.setFillStyle(this.darkBlue);
 		let unitHeight = FroggerMainLevelController.unitHeight;
 		let numUnits = Math.floor(this.transform.size.y / unitHeight);
 		for (let i=0; i<numUnits; i++) {
