696cb558bb76f0b2df0584b3ff89490d19bdb233
Nate Brooks <nateonguitar@gmail.com>
2019-05-27 00:38:12 -0600

    A little more code cleanup for demoing

--- Frogger/Levels/FroggerMainLevelController.ts
+++ Frogger/Levels/FroggerMainLevelController.ts
@@ -17,10 +17,9 @@ class FroggerMainLevelController extends GameObject {
 		this.player = new FroggerPlayer(unitHeight * 10);
 		this.river = new FroggerRiver(unitHeight * 10);
 
+		Debug.track(this.player);
 		this.buildLogs();
 
-		Debug.track(this.player);
-		Debug.track(this.logs[0]);
 	}
 
 	// override
@@ -30,16 +29,16 @@ class FroggerMainLevelController extends GameObject {
 
 	private buildLogs(): void {
 		let unitHeight = FroggerMainLevelController.unitHeight;
-		let log1 = new FroggerLogSmall(new Vector2(this.player.transform.position.x, 0));
-		log1.transform.position.y = this.river.transform.position.y + unitHeight*6 - log1.transform.size.y/2;
-		this.logs.push(log1);
 
-		let log2 = new FroggerLogSmall(new Vector2(this.player.transform.position.x, 0));
-		log2.transform.position.y = this.river.transform.position.y + unitHeight*8 - log2.transform.size.y/2;
-		this.logs.push(log2);
+		for (let i=0; i<3; i++) {
+			let log = new FroggerLogSmall(new Vector2(this.player.transform.position.x, 0));
+			log.transform.position.y = this.river.transform.position.y + unitHeight*i*2 + unitHeight*4 - log.transform.size.y/2;
+			this.logs.push(log);
+		}
 
 		for (let l of this.logs) {
 			l.setCollider();
+			Debug.track(l);
 		}
 	}
 }
