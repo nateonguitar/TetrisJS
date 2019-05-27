aaf27bf201fd1a674bc402836919edaaf54c831d
Nate Brooks <nateonguitar@gmail.com>
2019-05-27 10:11:15 -0600

    Abstracted detecting if a GameObject is in view of the camera.

--- lib/Camera.ts
+++ lib/Camera.ts
@@ -21,4 +21,17 @@ class Camera {
 			this.position = o.transform.position
 		}
 	}
+
+	public inViewOf(gameObject: GameObject): boolean {
+		let screenWidth = GameManager.options.screenWidth;
+		let screenHeight = GameManager.options.screenHeight;
+		let pos = gameObject.transform.position;
+		let offset = gameObject.transform.size.scale(0.5);
+		return (
+			pos.x + offset.x > this.position.x - screenWidth/2 &&
+			pos.y + offset.y > this.position.y - screenHeight/2 &&
+			pos.x - offset.x < this.position.x + screenWidth/2 &&
+			pos.y - offset.y < this.position.y + screenHeight/2
+		);
+	}
 }
