338e6c44774940e235f0012102ad5146a0b76428
nate brooks <nateonguitar@gmail.com>
2019-06-09 22:04:47 -0600

    found spritesheet and created base classes to run mario, no objects yet

---
+++ Mario/Levels/MarioLevel01.ts
@@ -0,0 +1,11 @@
+class MarioLevel01 extends Level {
+    constructor() {
+		super(<LevelParams>{
+			managingGameObjectClass: MarioLevel01Controller,
+			imageSrcs: [
+				'Mario/Images/SpriteSheet.png',
+            ],
+			unitSize: 50,
+		});
+	}
+}
