e7e2b9516b890239c5a05b427a9d58c486473764
Nate Brooks <nateonguitar@gmail.com>
2019-06-15 16:52:15 -0600

    since draws down as y goes more positive, we flip only the y to make it feel more natural.

--- lib/GameObject.ts
+++ lib/GameObject.ts
@@ -73,8 +73,8 @@ class GameObject {
 	public colliderPosition(): Vector2 {
 		let t = this.transform;
 		let colPos = this.collider.position.clone();
-		colPos.x *= -t.size.x;
-		colPos.y *= -t.size.y;
+		colPos.x *= t.size.x;
+		colPos.y *= -t.size.y; // flipping to feel more natural to users
 		return t.position.add(colPos);
 	}
 
