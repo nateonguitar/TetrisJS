e3f30e7b4712df8d5e7622adfb4a0b81002b6656
Nate Brooks <nateonguitar@gmail.com>
2019-06-15 11:42:35 -0600

    implemented solid objects with allowPassThrough = false on colliders

--- lib/Collider.ts
+++ lib/Collider.ts
@@ -2,13 +2,19 @@ interface ColliderParams {
 	position: Vector2;
 	/** size defaults to `new Vector2(1, 1)` in RectCollider */
 	size: Vector2;
+	allowPassThrough?: boolean;
 }
 
 class Collider {
 
 	public position: Vector2;
+	public allowPassThrough = true;
 
 	constructor(params:ColliderParams) {
 		this.position = params.position.clone();
+
+		if (params.allowPassThrough === false) {
+			this.allowPassThrough = false;
+		}
 	}
 }
