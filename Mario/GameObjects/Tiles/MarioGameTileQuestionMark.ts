df63c1ded82af636be04cbe122420f5da2ab7521
Nate Brooks <nateonguitar@gmail.com>
2019-06-18 20:06:17 -0600

    Question mark handles its own logic and now goes to a block that can't be broken once spent

--- Mario/GameObjects/Tiles/MarioGameTileQuestionMark.ts
+++ Mario/GameObjects/Tiles/MarioGameTileQuestionMark.ts
@@ -1,6 +1,13 @@
 class MarioGameTileQuestionMark extends MarioGameTile {
 
+	private spent: boolean = false;
+	private flashing: boolean = false;
 
+	private idleDuration = 2000;
+	private idleStartTime = 0;
+
+	private flashDuration = 400;
+	private flashStartTime = 0;
 
 	constructor() {
 		super({
@@ -21,24 +28,49 @@ class MarioGameTileQuestionMark extends MarioGameTile {
 						transforms: [
 							new Transform(new Vector2(MarioGameTile.spriteSize.x * 25, 0), MarioGameTile.spriteSize),
 							new Transform(new Vector2(MarioGameTile.spriteSize.x * 26, 0), MarioGameTile.spriteSize),
+							new Transform(new Vector2(MarioGameTile.spriteSize.x * 24, 0), MarioGameTile.spriteSize),
 						],
 						msPerFrame: 200
 					}),
 					"spent": new SpritesheetAnimation({
 						imageSrc: MarioGameTile.spriteSheet,
-						transforms: [ new Transform(new Vector2(0, 0), MarioGameTile.spriteSize), ],
+						transforms: [ new Transform(new Vector2(MarioGameTile.spriteSize.x * 3, 0), MarioGameTile.spriteSize), ],
 						msPerFrame: 10000,
+						loop: false
 					}),
 				},
 				"idle" // start animation name
 			)
 		});
+		this.idleStartTime = Time.time;
+	}
+
+	update(): void {
+		if (!this.spent) {
+			if (this.flashing) {
+				if (this.flashStartTime + this.flashDuration <= Time.time) {
+					this.flashing = false;
+					this.spritesheetAnimationSet.currentAnimationName = "idle";
+					this.idleStartTime = Time.time;
+				}
+			}
+			else {
+				if (this.idleStartTime + this.idleDuration <= Time.time) {
+					this.flashing = true;
+					this.spritesheetAnimationSet.currentAnimationName = "flash";
+					this.flashStartTime = Time.time;
+				}
+			}
+		}
 	}
 
 	// override
 	public onHitFromBeneath(): void {
-		console.log("Coin!");
-		let manager = <MarioLevelController> GameManager.currentLevel.managingGameObject;
-		manager.replaceTile(this, MarioGameTileBricksBrown);
+		if (!this.spent) {
+			this.spent = true;
+			console.log("Coin!");
+			this.spritesheetAnimationSet.currentAnimationName = "spent";
+		}
+
 	}
 }
