16d41dee7463cd1ec3f7ed6bad9dad8bb80d3240
Nate Brooks <nateonguitar@gmail.com>
2019-10-26 23:55:05 -0600

    Removed the need to have a huge list of keys

--- src/Input.ts
+++ src/Input.ts
@@ -5,81 +5,6 @@ import GameManager from "./GameManager";
 import Keys from "./Keys";
 
 export default class Input {
-	private static _KEYS: {[key:string]: string} = {
-		'Tab': Keys.Tab,
-		'ControlLeft': Keys.ControlLeft,
-		'ControlRight': Keys.ControlRight,
-		'AltLeft': Keys.AltLeft,
-		'AltRight': Keys.AltRight,
-		'ShiftLeft': Keys.ShiftLeft,
-		'ShiftRight': Keys.ShiftRight,
-		'Escape': Keys.Escape,
-		'Space': Keys.Space,
-		'ArrowLeft': Keys.ArrowLeft,
-		'ArrowUp': Keys.ArrowUp,
-		'ArrowRight': Keys.ArrowRight,
-		'ArrowDown': Keys.ArrowDown,
-		'Digit0': Keys.Key0,
-		'Digit1': Keys.Key1,
-		'Digit2': Keys.Key2,
-		'Digit3': Keys.Key3,
-		'Digit4': Keys.Key4,
-		'Digit5': Keys.Key5,
-		'Digit6': Keys.Key6,
-		'Digit7': Keys.Key7,
-		'Digit8': Keys.Key8,
-		'Digit9': Keys.Key9,
-		'KeyA': Keys.KeyA,
-		'KeyB': Keys.KeyB,
-		'KeyC': Keys.KeyC,
-		'KeyD': Keys.KeyD,
-		'KeyE': Keys.KeyE,
-		'KeyF': Keys.KeyF,
-		'KeyG': Keys.KeyG,
-		'KeyH': Keys.KeyH,
-		'KeyI': Keys.KeyI,
-		'KeyJ': Keys.KeyJ,
-		'KeyK': Keys.KeyK,
-		'KeyL': Keys.KeyL,
-		'KeyM': Keys.KeyM,
-		'KeyN': Keys.KeyN,
-		'KeyO': Keys.KeyO,
-		'KeyP': Keys.KeyP,
-		'KeyQ': Keys.KeyQ,
-		'KeyR': Keys.KeyR,
-		'KeyS': Keys.KeyS,
-		'KeyT': Keys.KeyT,
-		'KeyU': Keys.KeyU,
-		'KeyV': Keys.KeyV,
-		'KeyW': Keys.KeyW,
-		'KeyX': Keys.KeyX,
-		'KeyY': Keys.KeyY,
-		'KeyZ': Keys.KeyZ,
-		'F1': Keys.F1,
-		'F2': Keys.F2,
-		'F3': Keys.F3,
-		'F4': Keys.F4,
-		'F5': Keys.F5,
-		'F6': Keys.F6,
-		'F7': Keys.F7,
-		'F8': Keys.F8,
-		'F9': Keys.F9,
-		'F10': Keys.F10,
-		'F11': Keys.F11,
-		'F12': Keys.F12,
-		'Backquote': Keys.Backquote,
-		'Slash': Keys.Slash,
-		'Period': Keys.Period,
-		'Comma': Keys.Comma,
-		'Quote': Keys.Quote,
-		'Semicolon': Keys.Semicolon,
-		'BracketRight': Keys.BracketRight,
-		'BracketLeft': Keys.BracketLeft,
-		'Backslash': Keys.Backslash,
-		'Backspace': Keys.Backspace,
-		'Equals': Keys.Equals,
-		'Minus': Keys.Minus,
-	};
 
 	private static _keyDowns: { [key:string]:boolean } = {}
 
@@ -155,11 +80,11 @@ export default class Input {
 
 
 	private static getKeyDown(e:KeyboardEvent): void {
-		Input._keyDowns[Input._KEYS[e.code]] = true;
+		Input._keyDowns[Keys[e.code]] = true;
 	}
 
 	private static getKeyUp(e: KeyboardEvent): void {
-		Input._keyDowns[Input._KEYS[e.code]] = false;
+		Input._keyDowns[Keys[e.code]] = false;
 	}
 
 	public static registerMouseDown(toBind: object, func:Function): void {
